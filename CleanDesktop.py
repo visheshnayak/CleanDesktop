48dbf8c99d87bdc7c34ba76b99d21bcc55601d0b
Vishesh Nayak <visheshn@nseit.com>
2018-10-05 11:51:13 +0530

    Working Base

--- CleanDesktop.py
+++ CleanDesktop.py
@@ -5,7 +5,7 @@ from os import walk
 #This scipt clears the desktop and relocates the files according to the name
 
 #defines the path
-desktopPath = 'C:\\Users\\visheshn\\Desktop'
+desktopPath = 'C:\\Users\\visheshn\\Desktop\\'
 
 listOfEntities = []
 
@@ -16,6 +16,21 @@ for (dirpath, dirnames, filenames) in walk(desktopPath):
 
 for file in listOfEntities :
 	#conditions according to the prefixes
-	#DBScript for training
-	if file[6:] == 'DBS_T_':
-		pass
+
+	#DBScript for work
+	if file[:6] == 'DBS_W_':
+		shutil.move(desktopPath + file, 'D:\\Vishesh\\Work\\DB\\Scripts\\' + file)
+		print ('Moved ' + file)
+	
+	#Document for work
+	elif file[:6] == 'DOC_W_':
+		shutil.move(desktopPath + file, 'D:\\Vishesh\\Work\\Documents\\' + file)
+		print ('Moved ' + file)
+	
+	#DBScript for Training 
+	elif file[:6] == 'DBS_T_':
+		shutil.move(desktopPath + file, 'D:\\Vishesh\\Training\\DB\\' + file)
+		print ('Moved ' + file)
+	
+	else:
+		print('Uncategorized ' + file)
